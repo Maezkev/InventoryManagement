7e447b63e2ef4dde1d7c4fb388dc3a8a275d10b8
Maezkev <58494367+Maezkev@users.noreply.github.com>
2020-04-26 15:44:08 +0700

    Initial commit

---
+++ app/src/main/java/com/maezkev/inventorymanagement/AddInventoryActivity.kt
@@ -0,0 +1,13 @@
+package com.maezkev.inventorymanagement
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.add_inventory.*
+
+class AddInventoryActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.add_inventory)
+    }
+}
