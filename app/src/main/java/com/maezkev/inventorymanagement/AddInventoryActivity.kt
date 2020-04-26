8bb9517f666a5c5f9766adfbed3f2df423f19511
Maezkev <58494367+Maezkev@users.noreply.github.com>
2020-04-26 16:08:26 +0700

    Change ID Name

--- app/src/main/java/com/maezkev/inventorymanagement/AddInventoryActivity.kt
+++ app/src/main/java/com/maezkev/inventorymanagement/AddInventoryActivity.kt
@@ -1,7 +1,10 @@
 package com.maezkev.inventorymanagement
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.provider.ContactsContract
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.add_inventory.*
 
@@ -9,5 +12,10 @@ class AddInventoryActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.add_inventory)
+
+        val Database_Val = getSharedPreferences("Inventory_Db", Context.MODE_PRIVATE)
+        Database_Val.edit().apply {
+            
+        }
     }
 }
--- app/src/main/java/com/maezkev/inventorymanagement/MainActivity.kt
+++ app/src/main/java/com/maezkev/inventorymanagement/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.maezkev.inventorymanagement
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log.d
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -11,8 +13,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        gotoAddInventory.setOnClickListener {
+        AddInventory_Button.setOnClickListener {
             startActivity(Intent(this, AddInventoryActivity::class.java))
         }
+        val Preferences_Val = getSharedPreferences("InventoryDb", Context.MODE_PRIVATE)
+        val SavedName_Val = Preferences_Val.getString("SaveProductName","This thing doesn't exist.")
+        d("Maezkev", "The Saved Message is : $SavedName_Val")
     }
 }
